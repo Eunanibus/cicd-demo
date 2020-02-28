f9aa0910a8ccfed4e1a861891b9b45d36d912ff9
ecamilleri <eunancamilleri@hotmail.com>
2020-02-27 16:52:25 -0800

    Initial Commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,20 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders taunt text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Eunan was here./i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders controversial opinion', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/React > Angular/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders all arguments mute', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/You can't change my mind./i);
   expect(linkElement).toBeInTheDocument();
 });
