a770dbcf19ca15bff74c1846ab80b6449318bb4c
ecamilleri <eunancamilleri@hotmail.com>
2020-02-27 19:26:26 -0800

    Added ESLint & added to build stage

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Eunan was here.
-                </p>
-                <p>
-                    React > Angular
-                </p>
-                <p>
-                    You can't change my mind.
-                </p>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Eunan was here.
+        </p>
+        <p>
+          {'React > Angular'}
+        </p>
+        <p>
+          {'You can\'t change my mind.'}
+        </p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
